60e108dbc9086678121075f941f1f334bba0fada
michaelkammin <michael@michaelkammin.com>
2014-11-04 08:26:12 -0600

    Add Folder Structure and Interfaces
    I add folders for Interfaces, Implementations, and classes and started
    to rough out some of the interfaces.

---
+++ FranksZooGame/FranksZooGame/Implementations/ApplicationSessionServiceImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FranksZooGame.Classes;
+using FranksZooGame.Interfaces;
+
+namespace FranksZooGame.Implementations
+{
+    public class ApplicationSessionServiceImpl : IApplicationSessionService
+    {
+        private User[] _currentUsers;
+        private Game _currentGame;
+
+        public void SetCurrentUsers(Classes.User[] currentUsers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Classes.User[] GetCurrentUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCurrentGame(Classes.Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Classes.Game GetCurrentGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FranksZooGame/FranksZooGame/Interfaces/IApplicationSessionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FranksZooGame.Classes;
+
+namespace FranksZooGame.Interfaces
+{
+    interface IApplicationSessionService
+    {
+        void SetCurrentUsers(User[] currentUsers);
+        User[] GetCurrentUsers();
+        void SetCurrentGame(Game game);
+        Game GetCurrentGame();
+    }
+}
